fb8d8369689f599ccec5f0bc88e77711e01ba91b
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2024-03-01 13:37:30 -0500

    handled the initial loading of component without items

--- js/guidedtourView.js
+++ js/guidedtourView.js
@@ -26,98 +26,103 @@ define([
 
     preRender: function () {
       this.steps = this.model.get('_items');
-      this.steps.forEach(function (step) {
-        var img = new Image();
-        img.src = step._graphic.src;
-      });
-      const globals = Adapt.course.get('_globals');
-      var guidedtour = globals._components._guidedtour;
-      this.model.set('guidedtour', guidedtour);
+      if (this.steps && this.steps.length >= 2) {
+        this.model.set('active', true);
+        this.steps.forEach(function (step) {
+          var img = new Image();
+          img.src = step._graphic.src;
+        });
+        const globals = Adapt.course.get('_globals');
+        var guidedtour = globals._components._guidedtour;
+        this.model.set('guidedtour', guidedtour);
+      }
       this.render();
     },
 
     postRender: function () {
-      var guidedtour = this.model.get('guidedtour');
-      this.componentID = this.$el.attr('data-adapt-id');
+      if (this.model.get('active')) {
+        var guidedtour = this.model.get('guidedtour');
+        this.componentID = this.$el.attr('data-adapt-id');
 
-      this.tour = new Shepherd.Tour({
-        defaultStepOptions: {
-          cancelIcon: {
-            enabled: true
-          },
-          scrollTo: false
-        }
-      });
-
-
-      this.verifyCompletion = function () {
-        if (Object.values(this.steps).every(step => step.inView === true)) {
-          this.setCompletionStatus();
-        }
-      }
-
-      this.previousStep = function (self, stepIndex) {
-        var step = this.steps[stepIndex];
-        this.loadImage(step._graphic.src).then(() =>
-          self.back()
-        );
-      };
-
-      this.nextStep = function (self, stepIndex) {
-        this.steps[stepIndex].inView = true;
-        var step = this.steps[stepIndex];
-        this.loadImage(step._graphic.src).then(() =>
-          self.next()
-        );
-      };
-
-      this.loadImage = function (src) {
-        return new Promise((resolve, reject) => {
-          const img = this.$el.find(`.guidedtour-graphic img`)[0]
-          img.onload = () => resolve(img);
-          img.onerror = reject;
-          img.src = src;
-        })
-      };
-
-      var self = this;
-
-      this.tour.on('cancel', function (e) {
-        self.loadImage(self.steps[0]._graphic.src).then(() => {
-          self.$el.find('.guidedtour-graphic img').addClass('tour-disabled');
-          self.$el.find('.start-tour').removeClass('display-none');
-          self.verifyCompletion();
-        })
-      });
-
-      this.steps.forEach(function (step, index) {
-        var stepObject = {
-          title: step.title,
-          text: step.body,
-          buttons: [
-            {
-              action() {
-                return index === 0 ? self.tour.cancel() : self.previousStep(this, (index - 1));
-              },
-              classes: 'shepherd-button-secondary',
-              text: index === 0 ? guidedtour.closeText : guidedtour.previousText
+        this.tour = new Shepherd.Tour({
+          defaultStepOptions: {
+            cancelIcon: {
+              enabled: true
             },
-            {
-              action() {
-                return index === (self.steps.length - 1) ? self.tour.cancel() : self.nextStep(this, (index + 1));
-              },
-              text: index === (self.steps.length - 1) ? guidedtour.closeText : guidedtour.nextText
-            }
-          ],
-          id: `step-${index}-${self.componentID}`,
-          attachTo: {
-            element: `div[data-adapt-id="${self.componentID}"] .tour-item-${index}`,
-            on: step._pin._bubbledirection !== 'none' ? step._pin._bubbledirection : 'bottom'
-          },
-          arrow: step._pin._bubbledirection !== 'none'
+            scrollTo: false
+          }
+        });
+
+
+        this.verifyCompletion = function () {
+          if (Object.values(this.steps).every(step => step.inView === true)) {
+            this.setCompletionStatus();
+          }
         }
-        self.tour.addStep(stepObject);
-      })
+
+        this.previousStep = function (self, stepIndex) {
+          var step = this.steps[stepIndex];
+          this.loadImage(step._graphic.src).then(() =>
+            self.back()
+          );
+        };
+
+        this.nextStep = function (self, stepIndex) {
+          this.steps[stepIndex].inView = true;
+          var step = this.steps[stepIndex];
+          this.loadImage(step._graphic.src).then(() =>
+            self.next()
+          );
+        };
+
+        this.loadImage = function (src) {
+          return new Promise((resolve, reject) => {
+            const img = this.$el.find(`.guidedtour-graphic img`)[0]
+            img.onload = () => resolve(img);
+            img.onerror = reject;
+            img.src = src;
+          })
+        };
+
+        var self = this;
+
+        this.tour.on('cancel', function (e) {
+          self.loadImage(self.steps[0]._graphic.src).then(() => {
+            self.$el.find('.guidedtour-graphic img').addClass('tour-disabled');
+            self.$el.find('.start-tour').removeClass('display-none');
+            self.verifyCompletion();
+          })
+        });
+
+        this.steps.forEach(function (step, index) {
+          var stepObject = {
+            title: step.title,
+            text: step.body,
+            buttons: [
+              {
+                action() {
+                  return index === 0 ? self.tour.cancel() : self.previousStep(this, (index - 1));
+                },
+                classes: 'shepherd-button-secondary',
+                text: index === 0 ? guidedtour.closeText : guidedtour.previousText
+              },
+              {
+                action() {
+                  return index === (self.steps.length - 1) ? self.tour.cancel() : self.nextStep(this, (index + 1));
+                },
+                text: index === (self.steps.length - 1) ? guidedtour.closeText : guidedtour.nextText
+              }
+            ],
+            id: `step-${index}-${self.componentID}`,
+            attachTo: {
+              element: `div[data-adapt-id="${self.componentID}"] .tour-item-${index}`,
+              on: step._pin._bubbledirection !== 'none' ? step._pin._bubbledirection : 'bottom'
+            },
+            arrow: step._pin._bubbledirection !== 'none'
+          }
+          self.tour.addStep(stepObject);
+        })
+      }
       this.$('.guidedtour-widget').imageready(this.setReadyStatus.bind(this));
       if (this.model.get('_setCompletionOn') === 'inview') {
         this.setupInviewCompletion('.component-widget');
@@ -125,7 +130,9 @@ define([
     },
 
     remove: function () {
-      this.tour.complete();
+      if (this.model.get('active')) {
+        this.tour.complete();
+      }
       Backbone.View.prototype.remove.call(this);
     },
 
